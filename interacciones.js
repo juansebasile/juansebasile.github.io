f054e848aeb92fa7bb8aa9170106c64e74662464
juansebasile <126639279+juansebasile@users.noreply.github.com>
2023-03-01 00:28:36 -0300

    initial commit
    Muchas gracias por todo lo aprendido en el curso, la inexperiencia y el tiempo me jugaron en contra pero todos los fundamentos y temarios quedaron claros. Todo excelente!!

---
+++ interacciones.js
@@ -0,0 +1,8 @@
+/*
+Interacción al ingreso: pregunta el nombre y emite un saludo.
+*/
+
+let nombre = ""
+nombre = prompt("¡Hola! Bienvenido a mi página web, aquí encontrarás mi Curriculum vitae. ¿Cual es tu nombre?")
+alert ("Bienvenido " + nombre);
+
